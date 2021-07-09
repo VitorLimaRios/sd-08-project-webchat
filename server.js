6c3c158694a9d74c4b916c3d44de9a869e248a32
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-09 14:15:41 -0300

    Cria arquivos iniciais

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 // Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+
+io.on('connect', (socket) => {
+  console.log(`User connected! ID: ${socket.id}`);
+});
+
+app.get('/', (_req, res) => {
+  res.send('hello');
+});
+
+const PORT = 3000;
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
