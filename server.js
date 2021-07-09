ff8782a5704ae6949292970bb27cdc37f9579874
Tandy <tandyfw@gmail.com>
2021-07-09 15:53:31 -0300

    Commit inicial

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 // Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3002;
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+http.listen(PORT, () => console.log(`Servidor aberto na porta ${PORT}.`));
