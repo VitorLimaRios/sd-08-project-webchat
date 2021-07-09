7910a2a8054c7325bed526a0eaf07f82fd3dcf74
liisaambrosen <liisa.ambrosen@gmail.com>
2021-07-09 16:41:37 -0300

    Requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+
+const app = express();
+const http = require('http').createServer(app);
+require('dotenv/config');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+require('./sockets')(io);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+http.listen(process.env.PORT, () => {
+  console.log(`Ouvindo na porta ${process.env.PORT}`);
+});
