7e4e54fd73da042a36f46fedce5ec642e4cb750a
VitorLimaRios <vitorlimarios999@gmail.com>
2021-07-09 20:25:21 -0300

    iniciando projeto VQV

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} se conectou`);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const now = new Date();
+    const date = `${now.getDay()}-${now.getMonth()}-${now.getFullYear()}`;
+    const time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    const message = `${date} ${time} - ${nickname}: ${chatMessage}`;
+    io.emit('message', message);
+  });
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+http.listen(3000, () => { console.log('Ouvindo na porta 3000'); });
