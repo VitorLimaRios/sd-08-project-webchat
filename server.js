366a0eca3e80e92e55ed63cb41dabf4e7dc5197a
Fabio Ferreira <fabio35547@gmail.com>
2021-07-09 18:56:34 -0300

    Requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  } });
+
+require('./sockets/webchat')(io);
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
---
+++ sockets/webchat.js
@@ -0,0 +1,20 @@
+const formattedDate = () => {
+  const dateTimeNow = new Date();
+
+  return `${dateTimeNow.getDate()}-`
+  + `${dateTimeNow.getMonth() + 1}-`
+  + `${dateTimeNow.getFullYear()} `
+  + `${dateTimeNow.getHours()}:`
+  + `${dateTimeNow.getMinutes()}`;
+};
+
+module.exports = (io) => io.on('connection', (socket) => {
+  // console.log(`Usuário conectado com sucesso! SocketId: ${socket.id} `);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    console.log(chatMessage);
+
+    const message = `${formattedDate()} - ${nickname}: ${chatMessage}`;
+    io.emit('message', message);
+  });
+});
