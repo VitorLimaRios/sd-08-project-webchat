136fdb35ea617a571fbf0d740113a2877686fa38
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-07-09 12:35:24 -0300

    Evaluator test

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Faça seu código aqui
 const express = require('express');
-const app = express()
 
-const PORT = 3000
+const app = express();
+
+const PORT = 3000;
 
 app.listen(PORT, () => `Listening on PORT: ${PORT}`);
