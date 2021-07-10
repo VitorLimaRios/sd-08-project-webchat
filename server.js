31a65fd6f202c2a46696085a073589aae3797745
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-10 16:43:21 -0300

    4ll reqs

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-// Faça seu código aqui
+require('./sockets');
+
+const server = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
