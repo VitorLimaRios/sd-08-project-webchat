5892a494121038a3327cbcff0a01aaa4026d3f4d
enicio <jessandro42@gmail.com>
2021-07-10 00:38:29 -0300

    Requisito 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,36 @@
 const socket = window.io();
 
 const form = document.querySelector('form');
+let nickname;
+const addUserButton = document.querySelector('#addNickname');
+
+addUserButton.addEventListener('click', () => {
+  nickname = document.querySelector('#nickName').value;
+  document.querySelector('#nickNameOnline').innerText = nickname;
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nickname = document.querySelector('#nickName').value;
+
   const chatMessage = document.querySelector('#messageInput').value;
   socket.emit('message', { chatMessage, nickname });
   document.querySelector('#messageInput').value = '';
   return false;
 });
 
-const newUser = (nickName) => {
-  console.log(nickName);
+const newUser = (nickNameTemp) => {
+  // console.log(nickNameTemp);
+  document.querySelector('#nickNameOnline').innerText = nickNameTemp;
 };
 
 const createMessage = (data) => {
   // console.log('message', data);
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
   li.innerText = data;
   messagesUl.appendChild(li);
 };
 
 socket.on('message', (data) => createMessage(data));
-socket.on('newUser', (nickName) => newUser(nickName));
+socket.on('newUser', (nickNameTemp) => newUser(nickNameTemp));
--- sockets/chat.js
+++ sockets/chat.js
@@ -4,12 +4,12 @@ module.exports = (io) => io.on('connection', (socket) => {
   // console.log(`Usuario com ID ${socket.id}.`);
   const socketId = socket.id;
 
-  socket.broadcast.emit('newUser', socketId);
+  socket.emit('newUser', socketId.substring(0, 16));
 
   socket.on('message', (data) => {
     const { chatMessage, nickname } = data;
     const initialNickName = nickname || socketId.substring(0, 16);
-    console.log('Sockets', data);
+    // console.log('Sockets', data);
     const chatHour = getChatHour();
 
     const chatMessageComplete = `${chatHour} ${initialNickName} ${chatMessage}`;
