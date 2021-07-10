7cfd77f898af2a5f066b82ffce3fa3371814891a
enicio <jessandro42@gmail.com>
2021-07-10 16:06:07 -0300

    Requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,10 +3,14 @@ const socket = window.io();
 const form = document.querySelector('form');
 let nickname;
 const addUserButton = document.querySelector('#addNickname');
+const nickNameOnline = '#nickNameOnline';
 
 addUserButton.addEventListener('click', () => {
+  const actualNickName = document.querySelector(nickNameOnline).innerText;
+  console.log('atual', actualNickName);
   nickname = document.querySelector('#nickName').value;
-  document.querySelector('#nickNameOnline').innerText = nickname;
+  document.querySelector(nickNameOnline).innerText = nickname;
+  socket.emit('userChangeNickName', { actualNickName, newNickName: nickname });
 });
 
 form.addEventListener('submit', (e) => {
@@ -20,7 +24,24 @@ form.addEventListener('submit', (e) => {
 
 const newUser = (nickNameTemp) => {
   // console.log(nickNameTemp);
-  document.querySelector('#nickNameOnline').innerText = nickNameTemp;
+  document.querySelector(nickNameOnline).innerText = nickNameTemp;
+  socket.emit('toLeftPanel', nickNameTemp);
+};
+
+// const ops = [];
+
+const usersConnected = (userConnected) => {
+  console.log('ops', userConnected);
+  const localUser = document.querySelector('#nickNameOnline').innerText;
+  const usersToRender = userConnected.filter((element) => element !== localUser);
+  const ul = document.querySelector('#allUserLoggeds');
+  ul.innerHTML = '';
+  usersToRender.forEach((element) => {
+    const li = document.createElement('li');
+    li.setAttribute('data-testid', 'online-user');
+    li.innerText = element;
+    ul.appendChild(li);
+  });
 };
 
 const createMessage = (data) => {
@@ -34,3 +55,4 @@ const createMessage = (data) => {
 
 socket.on('message', (data) => createMessage(data));
 socket.on('newUser', (nickNameTemp) => newUser(nickNameTemp));
+socket.on('userConnecteds', (userConnected) => usersConnected(userConnected));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,11 +1,42 @@
 const { getChatHour } = require('../utils/service');
 
-module.exports = (io) => io.on('connection', (socket) => {
-  // console.log(`Usuario com ID ${socket.id}.`);
-  const socketId = socket.id;
+// const usersConnecteds = [];
+const usersLoggeds = [];
 
-  socket.emit('newUser', socketId.substring(0, 16));
+// const usersConnecteds = [];
 
+function userList(socket, io) {
+  socket.on('toLeftPanel', (tempUser) => {
+    usersLoggeds.push(tempUser);
+    console.log('Usuarios logados', usersLoggeds);
+    io.emit('userConnecteds', usersLoggeds);
+  });
+}
+
+function updateUserList(socket, io) {
+  socket.on('userChangeNickName', (nicknameObj) => {
+      console.log(nicknameObj);
+      const actualNicknamePosition = usersLoggeds.indexOf(nicknameObj.actualNickName);
+      console.log('position to update', actualNicknamePosition);
+      usersLoggeds.splice(actualNicknamePosition, 1);
+      usersLoggeds.push(nicknameObj.newNickName);
+      console.log('Apos upadte', usersLoggeds);
+      io.emit('userConnecteds', usersLoggeds);
+    });
+}
+
+function updateListOfLoggedUsersWhenDisconnect(socket, io) {
+  socket.on('disconnect', () => {
+    let ops = socket.id;
+    ops = ops.substring(0, 16);
+    const whoRemove = usersLoggeds.indexOf(ops);
+    usersLoggeds.splice(whoRemove, 1);
+    console.log(`desconectou ${ops} position ${whoRemove}`);
+    io.emit('userConnecteds', usersLoggeds);
+  });
+}
+
+function generateMessage(socket, io, socketId) {
   socket.on('message', (data) => {
     const { chatMessage, nickname } = data;
     const initialNickName = nickname || socketId.substring(0, 16);
@@ -15,4 +46,26 @@ module.exports = (io) => io.on('connection', (socket) => {
     const chatMessageComplete = `${chatHour} ${initialNickName} ${chatMessage}`;
     io.emit('message', chatMessageComplete);
   });
+}
+
+module.exports = (io) => io.on('connection', async (socket) => {
+  console.log(`Usuario com ID ${socket.id}.`);
+  const socketId = socket.id;
+  // const users = await io.of('/').adapter.sids;
+  // users.forEach((element) => {
+  //   const iterator = element.values();
+  //   const useres = iterator.next().value;
+  // });
+  // usersConnecteds.push(socketId.substring(0, 16));
+
+  io.emit('userConnecteds', usersLoggeds);
+
+  socket.emit('newUser', socketId.substring(0, 16));
+
+  userList(socket, io);
+
+  generateMessage(socket, io, socketId);
+  updateListOfLoggedUsersWhenDisconnect(socket, io);
+
+  updateUserList(socket, io);
 });
