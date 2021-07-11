e8f41d8985375b13a1bdef5f8c6ac0d5ea22ea90
enicio <jessandro42@gmail.com>
2021-07-11 00:20:52 -0300

    Requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,12 +4,14 @@ const form = document.querySelector('form');
 let nickname;
 const addUserButton = document.querySelector('#addNickname');
 const nickNameOnline = '#nickNameOnline';
+const dataTestid = 'data-testid';
 
 addUserButton.addEventListener('click', () => {
   const actualNickName = document.querySelector(nickNameOnline).innerText;
   console.log('atual', actualNickName);
   nickname = document.querySelector('#nickName').value;
   document.querySelector(nickNameOnline).innerText = nickname;
+  localStorage.setItem('nickname', nickname);
   socket.emit('userChangeNickName', { actualNickName, newNickName: nickname });
 });
 
@@ -17,28 +19,37 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const chatMessage = document.querySelector('#messageInput').value;
+  nickname = document.querySelector(nickNameOnline).innerText;
+  console.log(chatMessage, nickname);
   socket.emit('message', { chatMessage, nickname });
   document.querySelector('#messageInput').value = '';
   return false;
 });
 
 const newUser = (nickNameTemp) => {
-  // console.log(nickNameTemp);
-  document.querySelector(nickNameOnline).innerText = nickNameTemp;
-  socket.emit('toLeftPanel', nickNameTemp);
+  console.log('Nome de inicio', nickNameTemp);
+  const localNick = localStorage.getItem('nickname');
+  console.log('--------------Local', localNick);
+  document.querySelector(nickNameOnline).innerText = localNick || nickNameTemp;
+  // const user = localNick || nickNameTemp;
+  // socket.emit('toLeftPanel', user);
+  socket.emit('toLeftPanel', localNick || nickNameTemp);
+  // if (localNick) {
+  //   socket.emit('toLeftPanel', localNick);
+  // } else {
+  //   socket.emit('toLeftPanel', nickNameTemp);
+  // }
 };
 
-// const ops = [];
-
 const usersConnected = (userConnected) => {
-  console.log('ops', userConnected);
+  console.log('Conectados', userConnected);
   const localUser = document.querySelector('#nickNameOnline').innerText;
   const usersToRender = userConnected.filter((element) => element !== localUser);
   const ul = document.querySelector('#allUserLoggeds');
   ul.innerHTML = '';
   usersToRender.forEach((element) => {
     const li = document.createElement('li');
-    li.setAttribute('data-testid', 'online-user');
+    li.setAttribute(dataTestid, 'online-user');
     li.innerText = element;
     ul.appendChild(li);
   });
@@ -48,11 +59,25 @@ const createMessage = (data) => {
   // console.log('message', data);
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
-  li.setAttribute('data-testid', 'message');
+  li.setAttribute(dataTestid, 'message');
   li.innerText = data;
   messagesUl.appendChild(li);
+};
+
+const renderChat = (tramela) => {
+  console.log('chatlocal', tramela);
+  const messagesUl = document.querySelector('#messages');
+
+  tramela.forEach((element) => {
+    const { timestamp, nickname: opsq, message } = element;
+    const li = document.createElement('li');
+    li.setAttribute(dataTestid, 'message');
+    li.innerText = `${timestamp} ${opsq} ${message}`;
+    messagesUl.appendChild(li);
+  });
 };
 
 socket.on('message', (data) => createMessage(data));
 socket.on('newUser', (nickNameTemp) => newUser(nickNameTemp));
 socket.on('userConnecteds', (userConnected) => usersConnected(userConnected));
+socket.on('allchats', (tramela) => renderChat(tramela));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,32 +1,60 @@
 const { getChatHour } = require('../utils/service');
+const connection = require('../models/connection');
 
 // const usersConnecteds = [];
 const usersLoggeds = [];
+let objstest = {};
 
 // const usersConnecteds = [];
 
+async function findHistoryChatByUser() {
+  const allChat = connection().then((db) => db.collection('messages').find({ }).toArray());
+  const allChatFinded = await allChat;
+  return allChatFinded;
+}
+
+function createChat(chatHour, nickname, chatMessage) {
+  connection().then((db) => db.collection('messages')
+  .insertOne({ message: chatMessage, timestamp: chatHour, nickname }));
+}
+
 function userList(socket, io) {
-  socket.on('toLeftPanel', (tempUser) => {
+  socket.on('toLeftPanel', async (tempUser) => {
     usersLoggeds.push(tempUser);
     console.log('Usuarios logados', usersLoggeds);
+    const tramela = await findHistoryChatByUser();
+    socket.emit('allchats', tramela);
     io.emit('userConnecteds', usersLoggeds);
   });
 }
 
 function updateUserList(socket, io) {
-  socket.on('userChangeNickName', (nicknameObj) => {
+  socket.on('userChangeNickName', async (nicknameObj) => {
       console.log(nicknameObj);
+      const socketId = socket.id;
+      objstest = { [socketId]: nicknameObj.newNickName, ...objstest };
+
+      console.log(objstest);
+
       const actualNicknamePosition = usersLoggeds.indexOf(nicknameObj.actualNickName);
       console.log('position to update', actualNicknamePosition);
       usersLoggeds.splice(actualNicknamePosition, 1);
       usersLoggeds.push(nicknameObj.newNickName);
-      console.log('Apos upadte', usersLoggeds);
+      console.log('Apos update', usersLoggeds);
       io.emit('userConnecteds', usersLoggeds);
+      const tramela = await findHistoryChatByUser();
+      // console.log('os banco', tramela);
+      socket.emit('allchats', tramela);
     });
 }
 
 function updateListOfLoggedUsersWhenDisconnect(socket, io) {
   socket.on('disconnect', () => {
+    console.log('soliditante', socket.id);
+    const opsid = socket.id;
+    console.log('O array', usersLoggeds);
+    console.log('testao doido', objstest[opsid]);
+    const praremover = objstest[opsid];
     let ops = socket.id;
     ops = ops.substring(0, 16);
     const whoRemove = usersLoggeds.indexOf(ops);
@@ -37,11 +65,13 @@ function updateListOfLoggedUsersWhenDisconnect(socket, io) {
 }
 
 function generateMessage(socket, io, socketId) {
-  socket.on('message', (data) => {
+  socket.on('message', async (data) => {
     const { chatMessage, nickname } = data;
     const initialNickName = nickname || socketId.substring(0, 16);
     // console.log('Sockets', data);
     const chatHour = getChatHour();
+    console.log('On chat ----', chatHour, nickname, chatMessage);
+    await createChat(chatHour, nickname, chatMessage);
 
     const chatMessageComplete = `${chatHour} ${initialNickName} ${chatMessage}`;
     io.emit('message', chatMessageComplete);
