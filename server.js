f46f7996160e6fdf3fe6f13458cba2803d820137
Ana Karine <anakarine.ti@gmail.com>
2021-07-12 00:16:20 -0300

    nickname aleatorio

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-
+// const cors = require('cors');
+const path = require('path');
 const http = require('http').createServer(app);
 
-// const io = require('socket.io')(http, {
-//   cors: {
-//     origin: 'http://localhost:3000', // url aceita pelo cors
-//     methods: ['GET', 'POST'], // Métodos aceitos pela url
-//   },
-// });
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
     
 const ChatController = require('./controllers/chatController');
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+const clients = [];
+
+io.on('connection', (socket) => {
+  socket.on('newUser', (nickname) => {
+    clients.push({ userId: socket.id, nickname });
+    // console.log(clients, "clients")
+    socket.broadcast.emit('newUser', nickname);
+  });
+  
+  socket.on('onlineUsers', () => io.emit('onlineUsers', clients));
+});
 
 app.get('/', async (_req, res) => { 
     const messages = await ChatController.getAll();
--- views/js/script.js
+++ views/js/script.js
@@ -1,6 +1,46 @@
-// https://github.com/tryber/sd-07-live-lectures/blob/lecture_32_3/chat-socket-io/public/js/client.js
 const client = window.io();
-        
+
+const activeUsers = document.querySelector('.usersOn');
+
+// https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+function randomStringGenerator(size) {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < size; i += 1) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+}
+
+window.onload = () => {
+  const randomNickname = randomStringGenerator(16);
+  client.emit('newUser', randomNickname);
+  localStorage.setItem('user', JSON.stringify(randomNickname));
+
+  client.emit('onlineUsers', '');
+};
+
+client.on('onlineUsers', (clients) => {
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  clients.sort((a, b) => {
+    if (a.nickname > b.nickname) {
+      return 1;
+    }
+    if (a.nickname < b.nickname) {
+      return -1;
+    }
+    return 0;
+  });
+  // console.log(clients, "ordered clients");
+
+  clients.forEach((cli) => {
+    const userElement = document.createElement('li');
+    userElement.setAttribute('data-testid', 'online-user');
+    userElement.innerText = cli.nickname;
+    activeUsers.appendChild(userElement);
+  });
+});
+
 const createMessage = (message) => {
   const messageElement = document.createElement('li');
   messageElement.classList.add('msg');
