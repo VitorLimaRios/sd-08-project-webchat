3485e16dde2febb33ae1aa2a76a81b44773fd8d9
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-12 19:11:56 -0300

    Refatora requisito 01

--- sockets/messages.js
+++ sockets/messages.js
@@ -4,15 +4,16 @@ const { formatDate } = require('../services/formatDate');
 module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.on('message', async ({ chatMessage, nickname }) => {
-      const date = formatDate(new Date(Date.now()));
-      await messageModel.createMessage(date, nickname, chatMessage);
-      const message = `${date} - ${nickname}: ${chatMessage}`;
+      const timestamp = formatDate(new Date(Date.now()));
+      await messageModel.createMessage(timestamp, nickname, chatMessage);
+      const message = `${timestamp} - ${nickname}: ${chatMessage}`;
+      console.log(message, 'message');
       io.emit('message', message);
     });
 
     socket.on('getMessagesHistory', async () => {
       const messages = await messageModel.getMessage();
-
+      console.log(messages, 'messagesArray');
       socket.emit('messageHistory', messages);
     });
   });
