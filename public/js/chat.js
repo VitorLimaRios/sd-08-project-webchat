ea3ea3f3878852c37c2087fbdaa8083438fd9bc8
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-07-13 18:03:43 -0300

    Req 2 done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,54 @@
 const socket = window.io();
 const nicknameInput = document.getElementById('nickname-input');
 const messagesList = document.getElementById('messages');
+const nicknamesList = document.querySelector('.users-list');
 
-let nickName = '';
+const mySocket = { id: '' };
+const nicknames = {};
 
-socket.on('welcome', (message) => console.log(message));
-socket.on('defaultNickname', (defaultNick) => {
-  nickName = defaultNick;
-  nicknameInput.value = nickName;
+socket.on('socketId', (id) => {
+  mySocket.id = id;
+  // console.log(id);
 });
+
+socket.on('allNicknames', (allNicks) => {
+  const socketIds = Object.keys(allNicks);
+  // console.log(socketIds);
+  socketIds.forEach((socketId) => {
+    nicknames[socketId] = allNicks[socketId];
+  });
+  console.log(nicknames);
+  updateNicksOnScreen();
+});
+
+socket.on('updateNickname', ({ userId, newNick }) => {
+  nicknames[userId] = newNick;
+  console.log('UPDATED_NICKNAMES', nicknames);
+  updateNicksOnScreen();
+});
+
+const createUserNicknameLi = (username) => {
+  const li = document.createElement('li');
+  li.className = 'nickname-li';
+  li.innerHTML = username;
+  li.setAttribute('data-testid', 'online-user');
+  return li;
+};
+const updateNicksOnScreen = () => {
+  nicknamesList.innerHTML = '';
+const { [mySocket.id]: myId, ...otherNicks } = nicknames;
+const myNickOnScreen = document.querySelector('.my-nickname');
+myNickOnScreen.innerHTML = myId;
+Object.values(otherNicks).forEach((nick) => {
+nicknamesList.appendChild(createUserNicknameLi(nick));
+});
+};
 
 const createMessageLi = (message) => {
   const li = document.createElement('li');
   li.className = 'message-li';
   li.innerHTML = message;
+  li.setAttribute('data-testid', 'message');
   return li;
 };
 
@@ -21,18 +56,21 @@ socket.on('message', (message) => {
   messagesList.appendChild(createMessageLi(message));
 });
 
+socket.on('removeUser', (socketId) => { delete nicknames[socketId]; });
+
 const changeNicknameBtn = document.getElementById('save-nickname-btn');
 const updateNickname = (event) => {
   event.preventDefault();
-  nickName = nicknameInput.value;
-  socket.emit('updateNickname', nickName);
+  const newNickname = nicknameInput.value;
+  socket.emit('changeNickname', newNickname);
 };
 changeNicknameBtn.addEventListener('click', updateNickname);
 
 const sendMessage = (event) => {
+  const myNickname = nicknames[mySocket.id];
   event.preventDefault();
   const chatMessage = document.getElementById('message-input').value;
-  socket.emit('message', { chatMessage, nickName });
+  socket.emit('message', { chatMessage, nickname: myNickname });
 };
 
 const sendMessageBtn = document.getElementById('send-message');
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,24 +1,39 @@
 const moment = require('moment');
 // docs: https://momentjs.com/
 
-let nick;
+// let nick;
+const usersNicknames = {};
 
-module.exports = (io) => io.on(
-  'connection',
+const createUser = (socket) => {
+  const customId = socket.id.substring(0, 16);
+  usersNicknames[socket.id] = customId;
+  socket.emit('defaultNickname', customId);
+};
+
+const sendMessageToAll = (io, { chatMessage, nickname }) => {
+  const date = moment().format('DD-MM-YYYY');
+  const hour = moment().format('LTS');
+  const serverMessage = `${date} ${hour} - ${nickname}: ${chatMessage}`;
+  io.emit('message', serverMessage);
+};
+
+const changeNick = (socket, io, newNick) => {
+  usersNicknames[socket.id] = newNick;
+  io.emit('updateNickname', { userId: socket.id, newNick });
+};
+
+module.exports = (io) => {
+ io.on('connection',
   (socket) => {
-    const defaultNickname = socket.id.substring(0, 16);
-    socket.emit('welcome', `Welcome ${socket.id}`);
-    socket.emit('defaultNickname', defaultNickname);
-    console.log(`User ${socket.id} connected`);
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const date = moment().format('DD-MM-YYYY');
-    const hour = moment().format('LTS');
-    const serverMessage = `${date} ${hour} - ${nickname}: ${chatMessage}`;
-    io.emit('message', serverMessage);
+    // console.log(`User ${socket.id} connected`);
+    createUser(socket);
+    socket.emit('socketId', socket.id);
+    io.emit('allNicknames', usersNicknames);    
+    socket.on('changeNickname', (newNick) => { changeNick(socket, io, newNick); });
+    socket.on('message', (message) => { sendMessageToAll(io, message); });
+    socket.on('disconnect', (_reason) => {
+      delete usersNicknames[socket.id];
+      io.emit('removeUser', socket.id);
+    });
   });
-  socket.on('changeNickname', (newName) => {
-    nick = newName;
-    io.emit('updateNickname', { newNick: nick, user: defaultNickname });
-  });
-  },
-);
+};
