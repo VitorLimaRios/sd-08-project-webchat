e90ef0e3002254b15d593ecea9d5cb6e8305a640
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-13 11:33:33 -0300

    feat:  submitted  server on

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//   },
+// });
+
+const chatRouter = require('./routes/chatRouter');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(`${__dirname}/public`));
+
+app.use(chatRouter);
+app.use((err, _req, res, _next) => {
+  const { status, message } = err;
+  res.status(status).json({
+    message,
+  });
+});
+
+http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
