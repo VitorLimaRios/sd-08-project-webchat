ac06516bd61b3d39784ec43bb0d29cf446ef5a7b
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-13 17:05:53 -0300

    feat: send/ get message users

---
+++ public/js/chat.js
@@ -0,0 +1,34 @@
+const socket = window.io();
+
+const createElement = ({ tag, className, message }) => {
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  element.innerText = message;
+  return element;
+};
+
+const sendMessage = () => {
+  const form = document.getElementById('form_send');
+  form.addEventListener('submit', (e) => {
+  const message = e.target.children.message.value;
+  e.preventDefault();
+
+  socket.emit('message', {
+    message,
+    nickname: socket.id,
+  });
+
+  document.getElementById('message').value = '';
+});
+};
+
+const newMessage = (message) => {
+  const chat = document.getElementById('chat_message');
+  const p = createElement({ tag: 'p', className: 'user', message });
+  chat.appendChild(p);
+};
+
+window.onload = () => {
+  socket.on('message', (message) => newMessage(message));
+  sendMessage();
+};
