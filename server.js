0623cdf4d2683b9f3a51732ef58e9d7d537bbade
muriloPereira006 <muriloedu006@gmail.com>
2021-07-13 22:06:48 -0300

    iniciando pg e servidor

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const htpp = require('htpp').createServer(app);
+const cors = require('cors');
+const PORT = 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000/',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+http.listen(PORT, () => {
+  console.log(`Servidor rodando na ${PORT}`);
+})
