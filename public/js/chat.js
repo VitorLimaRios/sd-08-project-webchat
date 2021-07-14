6e3afc3859e7ff1f3f19d2ee246f47057f4024f1
Daniel <danieldsrd@gmail.com>
2021-07-13 23:44:44 -0300

    Teste requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,21 @@ const chat = document.querySelector('#chat');
 const message = document.querySelector('#message');
 const button = document.querySelector('#button');
 const nicknameInput = document.querySelector('#nickname');
-const nicknameOnline = document.querySelector('#username');
 const buttonNickname = document.querySelector('#buttonNick');
 const users = document.querySelector('#users');
+
+const DATA_TEST = 'data-testid';
+const ONLINE = 'online-user';
+
+const renderUsers = (user) => {
+  users.innerHTML = '';
+  console.log(user);
+  let li = document.createElement('li');
+  li = document.createElement('li');
+  li.innerText = user;
+  li.setAttribute(DATA_TEST, ONLINE);
+  users.appendChild(li);
+};
 
 function makeid(length) {
   let result = '';
@@ -15,46 +27,99 @@ function makeid(length) {
   for (let i = 0; i < length; i += 1) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  return result;
+  sessionStorage.setItem('nickname', result);
+  // renderUsers(result);
+  return socket.emit('user', result);
 } // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 
-window.onload = () => {
-  nicknameOnline.innerHTML = makeid(16);
-  localStorage.setItem('nickname', nicknameOnline.innerHTML); 
-};
+// const userList = [];
 
-buttonNickname.addEventListener('click', (e) => {
-  e.preventDefault();
-  localStorage.setItem('nickname', nicknameInput.value); 
-  socket.emit('user', nicknameInput.value);
+// const addUser = (username, sockets) => {
+//   const newUser = { username, sockets };
+//   console.log(newUser);
+//   userList.push(newUser);
+//   socket.emit('user', userList);
+// };
+
+// const updateNick = () => {
+//   const randomNick = makeid(16);
+//   sessionStorage.setItem('nickname', randomNick);
+//   users.innerHTML = randomNick;
+//   socket.on('user', () => {
+//     socket.emit('user', randomNick);
+//   });
+// };
+// updateNick();
+makeid(16);
+
+// nicknameOnline.innerHTML = makeid(16);
+// const randomNick = makeid(16);
+// sessionStorage.setItem('nickname', randomNick);
+// const newUser = { nickname: randomNick, socket };
+// userList.push(newUser);
+
+// const changeNick = (newNickname) => {
+//   console.log(newNickname);
+//   socket.emit('changeNick', { newNickname, socket });
+// };
+
+buttonNickname.addEventListener('click', () => {
+  sessionStorage.setItem('nickname', nicknameInput.value);  
+  const newNickname = sessionStorage.getItem('nickname');
+  const changeUser = { user: newNickname, socketId: socket.id };
+  console.log(changeUser);
+  socket.emit('changeNickName', changeUser);
   nicknameInput.value = '';
-  return false;
 });
 
 button.addEventListener('click', () => {
-  const nickname = localStorage.getItem('nickname');
+  const nickname = sessionStorage.getItem('nickname');
   const chatMessage = message.value;
   socket.emit('message', { chatMessage, nickname });
   message.value = '';
-  return false;
 });
 
 const renderMessage = (msg) => {
   const li = document.createElement('li');
-  li.setAttribute('data-testid', 'message');
+  li.setAttribute(DATA_TEST, 'message');
   li.innerText = msg;
   chat.appendChild(li);
 };
 
-const renderUsers = (user) => {
-  const li = document.createElement('li');
-  li.innerText = user;
-  li.setAttribute('data-testid', 'online-user');
-  users.appendChild(li);
+// const renderUsers = (user) => {
+//   users.innerHTML = '';
+//   console.log(user);
+//   let li = document.createElement('li');
+//   li = document.createElement('li');
+//   li.innerText = user;
+//   li.setAttribute(DATA_TEST, ONLINE);
+//   users.appendChild(li);
+// };
+
+const renderAllUsers = (user) => {
+  users.innerHTML = '';
+  const socketUser = user.find((currentUser) => currentUser.socketId === socket.id);
+  const otherUsers = user.filter((currentUser) => currentUser.socketId !== socket.id); 
+  let li = document.createElement('li');
+  [socketUser, ...otherUsers].forEach((elem) => {
+    li = document.createElement('li');
+    console.log(elem);
+    li.innerText = elem.user;
+    li.setAttribute(DATA_TEST, ONLINE);
+    users.appendChild(li);
+  });
 };
 
 socket.on('message', (msg) => {
   renderMessage(msg);
+});
+
+socket.on('changeNickName', (data) => {
+  renderAllUsers(data);
+});
+
+socket.on('allUsers', (data) => {
+  renderAllUsers(data);
 });
 
 socket.on('user', (msg) => {  
--- server.js
+++ server.js
@@ -25,32 +25,80 @@ const formatMessage = ({ time, nickname, message }) => (`${time} - ${nickname}: 
 app.use(bodyParser.json());
 app.use(cors());
 
+const usersList = [];
+
+const history = async () => {
+  const allMessages = await messageModel.findAllMessages();
+  const eachMessage = await allMessages.map((elem) => 
+    formatMessage({ time: elem.timestamp, nickname: elem.nickname, message: elem.message }));
+  return eachMessage;
+};
+
+const userDisconnect = (socket, users) => {
+  const userDiconnect = users.findIndex((user) => user.id === socket.id);
+  usersList.splice(userDiconnect, 1);
+};
+
+const addUser = (user, socket) => {
+  const socketId = socket.id;
+  usersList.push({ user, socketId });
+  if (usersList.length > 1) {
+    io.emit('allUsers', usersList);
+  } else {
+    io.emit('user', user);
+  }
+};
+
+const changeNickFunc = (data) => {
+  const findUser = usersList.find((user) => user.socketId === data.socketId);
+  findUser.user = data.user;
+  io.emit('changeNickName', usersList);
+};
+
+function makeid(length, socket) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  usersList.push({ user: result, socketId: socket.id });
+  return result;
+} // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
+const messageFormated = async (chatMessage, nickname) => {
+  const timeNow = dateFormat(now, 'dd-mm-yyyy h:MM:ss TT');
+  await messageModel.createMessage({ message: chatMessage, nickname, timestamp: timeNow });
+  const value = formatMessage({ time: timeNow, nickname, message: chatMessage });
+  return value;
+};
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(path.join(__dirname, '/public')));
 
 io.on('connection', async (socket) => {
-  console.log('Client connected');
+  // console.log(`connected ${socket.id}`);
+  const historyAll = await history();
+  io.emit('start', historyAll);
 
-  const allMessages = await messageModel.findAllMessages();
-  const eachMessage = await allMessages.map((elem) => 
-    formatMessage({ time: elem.timestamp, nickname: elem.nickname, message: elem.message }));  
-  io.emit('start', eachMessage);
+  socket.on('user', (user) => addUser(user, socket));
 
-  socket.on('user', (user) => {
-    io.emit('user', user);
-  });
+  socket.on('changeNickName', (data) => changeNickFunc(data));
 
   socket.on('message', async ({ chatMessage, nickname }) => {
-    const timeNow = dateFormat(now, 'dd-mm-yyyy h:MM:ss TT');
-    await messageModel.createMessage({ message: chatMessage, nickname, timestamp: timeNow });
-    const value = formatMessage({ time: timeNow, nickname, message: chatMessage });
+    const value = await messageFormated(chatMessage, nickname);
     io.emit('message', value);
+  });
+
+  socket.on('disconnect', () => {
+    userDisconnect(socket, usersList);
+    io.emit('allUsers', usersList);
   });
 });
 
 app.get('/', async (_req, res) => {
-  res.status(200).render('../views/chat.ejs');
+  res.render('chat');
 });
 
 http.listen(PORT, () => console.log(`App listening ${PORT}`));
