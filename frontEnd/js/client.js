5b74ddf22248b92c94ae88ac936c9c3c68294c62
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-07-16 10:43:39 -0300

    iniciando projeto

---
+++ frontEnd/js/client.js
@@ -0,0 +1,63 @@
+const socket = window.io();
+
+const messageForm = document.querySelector('#messagesForm');
+const nicknameForm = document.querySelector('#nicknameForm');
+
+const createMessagesList = (message) => {
+  const messagesList = document.querySelector('#messagesList');
+  const singleMessage = document.createElement('li');
+
+  singleMessage.setAttribute('data-testid', 'message');
+  singleMessage.setAttribute('class', 'msg');
+  singleMessage.innerText = message;
+  messagesList.appendChild(singleMessage);
+};
+
+const createUsersList = (user) => {
+  const usersList = document.querySelector('#usersList');
+  const usersName = document.querySelectorAll('#onlineUser');
+  if (usersName) usersName.forEach((item) => item.remove()); 
+
+  const nickname = sessionStorage.getItem('user');
+  
+  user.forEach((e) => {
+    const singleUser = document.createElement('li');
+    singleUser.setAttribute('data-testid', 'online-user');
+    singleUser.setAttribute('id', 'onlineUser');
+    singleUser.innerText = e;
+    if (nickname === e) {
+      usersList.prepend(singleUser);
+    } else {
+      usersList.appendChild(singleUser);
+    }
+  });
+};
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nickname = sessionStorage.getItem('user');
+  const chatMessage = document.querySelector('#messageBox').value;
+  socket.emit('message', { nickname, chatMessage });
+  return false;
+});
+
+nicknameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newNickname = document.querySelector('#nicknameBox').value;
+  socket.emit('changeNick', newNickname);
+  return false;
+});
+
+window.addEventListener('load', async () => {
+  const dbMessages = await (await fetch('http://localhost:3000/messages')).json();
+
+  dbMessages.forEach((e) => {
+    const message = `${e.timestamp} : ${e.nickname} say's: ${e.message}`;
+    createMessagesList(message);
+  });
+});
+
+socket.on('message', (newMessage) => createMessagesList(newMessage));
+socket.on('userNickname', (userNick) => createUsersList(userNick));
+socket.on('currentUser', (user) => sessionStorage.setItem('user', user));
+socket.on('userDisconnect', (userOut) => createUsersList(userOut));
--- server.js
+++ server.js
@@ -1,1 +1,66 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const dateFormat = require('dateformat');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const makeChar = (length) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+const chatModel = require('./models/chatModel');
+
+const chatUsers = {};
+const date = dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT');
+
+io.on('connection', (client) => { 
+   chatUsers[client.id] = `User_${makeChar(11)}`;
+  client.emit('currentUser', chatUsers[client.id]);
+  client.on('message', ({ nickname, chatMessage }) => {
+    chatModel.insertMessage({ message: chatMessage, nickname, timestamp: date });
+    const newMessage = `${date} - ${nickname}: ${chatMessage}`;
+    io.emit('message', newMessage);
+  });
+
+  io.emit('userNickname', Object.values(chatUsers));
+  client.on('changeNick', (nickname) => {
+    Object.values(chatUsers).filter((e) => e !== chatUsers[client.id]);
+    chatUsers[client.id] = nickname;
+    client.emit('currentUser', chatUsers[client.id]);
+    io.emit('userNickname', Object.values(chatUsers));
+  });
+
+  client.on('disconnect', () => {
+    delete chatUsers[client.id];
+    io.emit('userDisconnect', Object.values(chatUsers));
+  });
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/frontEnd`));
+
+app.get('/messages', async (_req, res) => {
+  try {
+    const messages = await chatModel.getAllMessages();
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+http.listen(3000, () => console.log('App listening on port 3000!'));
