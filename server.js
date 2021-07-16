bdac5d434c83d8c837ebcd6e4c07a7a10b878cf6
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-07-16 14:16:42 -0300

    iniciando projeto

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ const io = require('socket.io')(http, {
   },
 });
 
-const makeChar = (length) => {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
-
 const chatModel = require('./models/chatModel');
 
 const chatUsers = {};
