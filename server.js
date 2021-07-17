0a17d2480e33d337b4119339471b8773fa917a7b
Rosiele David <rosieleserpa@hotmail.com>
2021-07-17 18:19:32 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 // Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const { PORT = 3000 } = process.env;
+http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
