35af495b6ffbca50091b98145e551b2fd40fadae
Bruno Silva <bruno.silva@sistemas.newm.com.br>
2021-07-17 12:23:36 -0300

    teste

--- models/userChat.js
+++ models/userChat.js
@@ -1,13 +1,11 @@
 const { ObjectID } = require('mongodb');
 const connection = require('./connection');
 
-const getAll = () => {
+const getAll = () =>
   connection().then((db) => db.collection('users').find({}).toArray());
-};
   
-const addUser = (nickname) => {
+const addUser = (nickname) =>
   connection().then((db) => db.collection('users').insertOne({ nickname }));
-};
 
 const updateNickName = (id, newNickname) => {
   connection().then((db) =>
@@ -16,23 +14,20 @@ const updateNickName = (id, newNickname) => {
       .updateOne({ _id: ObjectID(id) }, { $set: { nickname: newNickname } }));
 };
 
-const addMessage = (message) => {
+const addMessage = (message) =>
   connection().then((db) => db.collection('messages').insertOne({ message }));
-};
 
-const getByName = (nickname) => {
-  connection().then((db) => db.collection('users').findOne({ nickname }));
-};
-
-const getAllMessages = async () => {
+const getAllMessages = async () =>
   connection().then((db) => db.collection('messages').find().toArray());
-};
+
+const getByName = (nickname) => 
+  connection().then((db) => db.collection('users').findOne({ nickname }));
 
 module.exports = {
   getAll,
   addUser,
   updateNickName,
   addMessage,
-  getByName,
   getAllMessages,
+  getByName,
 };
