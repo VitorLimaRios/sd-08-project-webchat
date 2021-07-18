58b65d3a7bb1362ecbf33db9cfb74c75f8b4627a
Anderson Santana <anderslash@gmail.com>
2021-07-17 22:16:35 -0300

    fix: correções no server, mudança nas funções

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const moment = require('moment');
 const cors = require('cors');
-const something = require('random-name'); // Referência Thays Costa
 
 const app = express();
 const http = require('http').createServer(app);
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const person = {};
 
 const io = require('socket.io')(http, {
   cors: {
@@ -14,34 +16,45 @@ const io = require('socket.io')(http, {
   },
 });
 
-const PORT = process.env.PORT || 3000;
-const person = {};
+const { saveMessage, fetchMessages } = require('./models/messages');
 
 const timestamp = () => {
   const time = moment().format('DD-MM-YYYY h:mm:ss A');
   return time;
 };
 
-io.on('connection', (client) => {
+const disconnect = (client) => {
+  const disconnectedPerson = person[client.id];
+  io.emit('exit', disconnectedPerson);
+  delete person[client.id];
+  io.emit('personList', person);
+  console.log(`${disconnectedPerson} left chat at ${timestamp()}`);
+};
+
+const message = async (nickname, chatMessage) => {
+  const content = `${timestamp()} - ${nickname}: ${chatMessage}`;
+  await saveMessage(chatMessage, nickname, timestamp());
+  io.emit('message', content);
+};
+
+io.on('connection', async (client) => {
   const { id } = client;
-  person[id] = something().slice(0, 16);
-  
-  const joinedPerson = person[id];
-  
-  console.log(`${joinedPerson} joined chat at ${timestamp()}`);
-  
-  client.on('message', ({ nickname, chatMessage }) => {
-    const content = `${timestamp()} - ${nickname}: ${chatMessage}`;
-    console.log(`${nickname} sent a message at ${timestamp()}`);
-    io.emit('message', content);
+  person[id] = id.slice(0, 16);
+  client.emit('setCurrentPerson', person[id]);
+  io.emit('personList', person);
+  const allMessages = await fetchMessages();
+  client.emit('listMessages', allMessages);
+
+  client.on('message', ({ nickname, chatMessage }) => message(nickname, chatMessage));
+
+  client.on('changePerson', (customPerson) => {
+    person[id] = customPerson;
+    io.emit('personList', person);
   });
-  
-   client.on('disconnect', () => {
-    const disconnectedPerson = person[id];
-    delete person[id];
-    console.log(`${disconnectedPerson} left chat at ${timestamp()}`);
-  });
+
+  client.on('disconnect', () => disconnect(client));
 });
 
 app.use(cors());
+app.use(express.static(`${__dirname}/public`));
 http.listen(PORT, () => console.log('Rodando liso na porta', PORT)); 
